cd30c6e4980e45ac952d68fffec2fcb151bef075
cuijunfeng <cuijf@haierubic.com>
2015-08-16 21:03:09 +0800

    调整架构

--- app/src/main/java/com/ilab/sur/tracker/MainActivity.java
+++ app/src/main/java/com/ilab/sur/tracker/MainActivity.java
@@ -1,38 +1,46 @@
 package com.ilab.sur.tracker;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+import com.ilab.lbs.baidu.BaiduLocationService;
+import com.ilab.lbs.baidu.BaiduMapView;
+import com.ilab.lbs.sur.location.SurLocation;
+import com.ilab.lbs.sur.location.SurLocationService;
+import com.ilab.lbs.sur.map.SurMap;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private MapView mapView = null;
+
+    private SurLocationService locationService = null;
+    private SurMap map = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(com.ilab.lbs.R.id.bmapView);
+
+        locationService = BaiduLocationService.getInstance(this);
+        map = new BaiduMapView(this, mapView);
+
+        locationService.registerLocationUpdateListener(new SurLocation.Listener() {
+            @Override
+            public void onLocationUpdate(SurLocation location) {
+                map.setLocation(location, true);
+            }
+        });
+        locationService.start();
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
